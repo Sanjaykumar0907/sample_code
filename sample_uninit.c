813d21a71eed80645e444a401ef35c8422a9e267
Sanjaykumar0907 <sanju.cmp.07@gmail.com>
2020-04-15 11:18:25 +0530

    Create sample_uninit.c
    this to demonstrate uninit vars

---
+++ sample_uninit.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int uninit() {
+
+   int i;
+   int j;
+   int k ;
+
+//  for ( i = 0; i< k; i++) { //
+
+if (k > 0) {
+ }
+   printf("%d", k);
+
+   i = i-1;
+   j = j-1;
+   if (k > 0) i = i+1;
+   else j=j+1;
+
+   i = i-1;
+   j = j-1;
+   return 0;
+}
